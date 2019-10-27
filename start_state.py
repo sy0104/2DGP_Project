068d899e7e037c6bf54a0b5c270b263dc810b0de
soyeong park <syys0104@gmail.com>
2019-10-28 03:37:43 +0900

    시작화면

---
+++ start_state.py
@@ -0,0 +1,39 @@
+import game_framework
+from pico2d import *
+import title_state
+
+name = "StartState"
+image = None
+logo_time = 0.0
+
+
+def enter():
+    global image
+    image = load_image('start_image.png')
+
+
+
+def exit():
+    global image
+    del(image)
+
+
+def update():
+    global logo_time
+
+    if logo_time > 1.0:
+        logo_time = 0
+        game_framework.change_state(title_state)
+    delay(0.01)
+    logo_time += 0.01
+
+
+def draw():
+    global image
+    clear_canvas()
+    image.draw(1280//2, 700//2)
+    update_canvas()
+
+def handle_events():
+    events = get_events()
+    pass
