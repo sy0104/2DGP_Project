0c5c68b31c25576f4f18a63ae789062b2b2a46ef
soyeong park <syys0104@gmail.com>
2019-11-19 01:53:08 +0900

    rect
    충돌시 이미지 변환

--- rect.py
+++ rect.py
@@ -1,15 +1,15 @@
 from pico2d import *
 import game_framework
 
-
 # 플레이어 캐릭터
 class Rect:
     def __init__(self):
-        self.image = load_image('Resource\\image\\player.png')
+        self.image_hp2 = load_image('Resource\\image\\player_hp2.png')
+        self.image_hp1 = load_image('Resource\\image\\player_hp1.png')
         self.x, self.y = 100, 300
         self.frame = 0
         self.time = 0
-        self.hp = 4
+        self.hp = 2
         self.dir_x = 0
         self.dir_y = 0
 
@@ -23,5 +23,14 @@ class Rect:
         pass
 
     def draw(self):
-        self.image.clip_draw(0, 0, 30, 30, self.x, self.y)
+        if self.hp == 2:
+            self.image_hp2.clip_draw(0, 0, 30, 30, self.x, self.y)
+        else:
+            self.image_hp1.clip_draw(0, 0, 15, 15, self.x, self.y)
+
+    def get_bb(self):
+        if self.hp == 2:
+            return self.x - 15, self.y - 15, self.x + 15, self.y + 15
+        else:
+            return self.x - 7.5, self.y - 7.5, self.x + 7.5, self.y + 7.5
 
