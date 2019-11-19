433b8a8317e0d7d4ee729b1645b5bb2538dfd45d
soyeong park <syys0104@gmail.com>
2019-11-20 03:00:21 +0900

    main_state
    enemy 추가 - 직선으로만 움직임(Stage1)

--- main_state.py
+++ main_state.py
@@ -6,7 +6,12 @@ from pico2d import *
 import game_framework
 import title_state
 import game_world
+import stage_1
+import stage2_image
 
+from Enemies import SmallestEnemy
+from Enemies import SmallEnemy
+from Enemies import BigEnemy
 from rect import Rect
 from stage_1 import BossFace
 
@@ -24,13 +29,22 @@ class Background:
         pass
 
 def enter():
-    global rect, background, stage_1
+    global rect, background, boss_face, SmallestEnemy, SmallEnemy, BigEnemy
     rect = Rect()
-    stage_1 = BossFace()
+    boss_face = BossFace()
+    SmallestEnemy = [SmallestEnemy() for i in range(15)]
+    SmallEnemy = [SmallEnemy() for j in range(10)]
+    BigEnemy = [BigEnemy() for k in range(7)]
     background = Background()
     game_world.add_object(background, 0)
     game_world.add_object(rect, 1)
-    game_world.add_object(stage_1, 1)
+    game_world.add_object(boss_face, 1)
+    for SmallestEnemy in SmallestEnemy:
+        game_world.add_object(SmallestEnemy, 0)
+    for SmallEnemy in SmallEnemy:
+        game_world.add_object(SmallEnemy, 0)
+    for BigEnemy in BigEnemy:
+        game_world.add_object(BigEnemy, 0)
 
     pass
 
@@ -63,6 +77,13 @@ def handle_events():
                 rect.dir_x += 2
             elif event.key == SDLK_LEFT:
                 rect.dir_x -= 2
+            # hp 설정
+            elif event.key == SDLK_p:
+                rect.hp = 2
+            elif event.key == SDLK_o:
+                rect.hp = 1
+
+
 
         elif event.type == SDL_KEYUP:
             if event.key == SDLK_UP or event.key == SDLK_DOWN:
@@ -72,13 +93,21 @@ def handle_events():
 
 
 
-
-
-
+time = 0
 def update():
+    global time
     for game_object in game_world.all_objects():
         game_object.update()
+    if collide(rect, boss_face):
+        rect.hp -= 1
+        print("COLLISION")
+
+    if time > 100.0:
+        logo_time = 0
+        game_framework.change_state(stage2_image)
+    time += 0.01
     pass
+
 
 def draw():
     clear_canvas()
@@ -87,6 +116,14 @@ def draw():
     update_canvas()
 
 
+def collide(a, b):
+    left_a, bottom_a, right_a, top_a = a.get_bb()
+    left_b, bottom_b, right_b, top_b = b.get_bb()
 
+    if left_a > right_b: return False
+    if right_a < left_b: return False
+    if top_a < bottom_b: return False
+    if bottom_a > top_b: return False
 
+    return True
 
