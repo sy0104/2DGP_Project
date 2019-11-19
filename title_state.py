e744e0340b87b2627851b1659890a79c39c0d5aa
soyeong park <syys0104@gmail.com>
2019-11-20 02:59:24 +0900

    Update title_state.py

--- title_state.py
+++ title_state.py
@@ -1,7 +1,7 @@
 import game_framework
 from pico2d import *
 import main_state
-import stage1_state
+import stage1_image
 
 name = "TitleState"
 image = None
@@ -26,7 +26,7 @@ def handle_events():
             if (event.type, event.key) == (SDL_KEYDOWN, SDLK_ESCAPE):
                 game_framework.quit()
             elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_RETURN):
-                game_framework.change_state(stage1_state)
+                game_framework.change_state(stage1_image)
 
 
 def draw():
