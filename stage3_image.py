9529571af0f913bf31e67e17a1bbbdfa395b0114
soyeong park <syys0104@gmail.com>
2019-11-20 17:05:18 +0900

    stage3_image
    stage3 예고화면

---
+++ stage3_image.py
@@ -0,0 +1,39 @@
+import game_framework
+from pico2d import *
+import stage_3
+
+name = "stage3_image"
+image = None
+logo_time = 0.0
+
+
+def enter():
+    global image
+    image = load_image('Resource\\image\\Stage_3.png')
+
+
+
+def exit():
+    global image
+    del(image)
+
+
+def update():
+    global logo_time
+
+    if logo_time > 0.5:
+        logo_time = 0
+        game_framework.change_state(stage_3)
+    delay(0.01)
+    logo_time += 0.01
+
+
+def draw():
+    global image
+    clear_canvas()
+    image.draw(1280//2, 700//2)
+    update_canvas()
+
+def handle_events():
+    events = get_events()
+    pass
