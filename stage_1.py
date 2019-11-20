1e09f5e497ef31716c4623bd00917ce5df0c5711
soyeong park <syys0104@gmail.com>
2019-11-20 17:05:58 +0900

    stage1
    enemy 추가 - 직선으로만 움직임

--- stage_1.py
+++ stage_1.py
@@ -2,90 +2,61 @@ import random
 import json
 import os
 
-import game_framework
 from pico2d import *
+import game_framework
 import title_state
-import stage2_state
+import game_world
+import stage2_image
 
-name = "stage_1"
-background = None
-rect = None
-boss_face = None
+from Enemies import SmallestEnemy
+from Enemies import SmallEnemy
+from Enemies import BigEnemy
+from rect import Rect
+from Boss import BossFace
+
+name = "MainState"
+
 
 # 배경
 class Background:
     def __init__(self):
         self.image = load_image('Resource\\image\\background_image.png')
-
     def draw(self):
         self.image.draw(1280//2, 700//2)
 
-
-# 플레이어 캐릭터
-class Rect:
-    def __init__(self):
-        self.image = load_image('Resource\\image\\player.png')
-        self.x, self.y = 100, 300
-        self.frame = 0
-        self.time = 0
-        self.hp = 4
-        self.dir_x = 0
-        self.dir_y = 0
-
-    def update(self):
-        self.x += self.dir_x
-        self.y += self.dir_y
-        if self.x >= 1265 or self.x <= 15:
-            self.dir_x = 0
-        if self.y >= 685 or self.y <= 15:
-            self.dir_y = 0
-        pass
-
-    def draw(self):
-        self.image.clip_draw(0, 0, 30, 30, self.x, self.y)
-
-# wave1 - 보스 얼굴
-class BossFace:
-    def __init__(self):
-        self.image = load_image('Resource\\image\\boss_face.png')
-        self.x, self.y = 650, 300
-
     def update(self):
         pass
-
-    def draw(self):
-        self.image.clip_draw(0, 0, 280, 280, self.x, self.y)
 
 def enter():
-    global background, rect, boss_face
-    background = Background()
+    global rect, background, boss_face, SmallestEnemy, SmallEnemy, BigEnemy
     rect = Rect()
     boss_face = BossFace()
+    SmallestEnemy = [SmallestEnemy() for i in range(15)]
+    SmallEnemy = [SmallEnemy() for j in range(10)]
+    BigEnemy = [BigEnemy() for k in range(7)]
+    background = Background()
+    game_world.add_object(background, 0)
+    game_world.add_object(rect, 1)
+    game_world.add_object(boss_face, 1)
+    for SmallestEnemy in SmallestEnemy:
+        game_world.add_object(SmallestEnemy, 0)
+    for SmallEnemy in SmallEnemy:
+        game_world.add_object(SmallEnemy, 0)
+    for BigEnemy in BigEnemy:
+        game_world.add_object(BigEnemy, 0)
+
+    pass
 
 def exit():
-    global background, rect, boss_face
-    del (background)
-    del (rect)
-    del (boss_face)
+    game_world.clear()
+    pass
 
 
-def draw():
-    clear_canvas()
-    background.draw()
-    rect.draw()
-    boss_face.draw()
-    update_canvas()
+def pause():
+    pass
 
-time = 0
 
-def update():
-    global time
-    rect.update()
-
-    if time > 10.0:
-        logo_time = 0
-        game_framework.change_state(stage2_state)
-    time += 0.01
+def resume():
     pass
 
 def handle_events():
@@ -93,6 +64,8 @@ def handle_events():
     for event in events:
         if event.type == SDL_QUIT:
             game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            game_framework.change_state(title_state)
         # 상하좌우 이동
         elif event.type == SDL_KEYDOWN:
             if event.key == SDLK_DOWN:
@@ -103,6 +76,13 @@ def handle_events():
                 rect.dir_x += 2
             elif event.key == SDLK_LEFT:
                 rect.dir_x -= 2
+            # hp 설정
+            elif event.key == SDLK_p:
+                rect.hp = 2
+            elif event.key == SDLK_o:
+                rect.hp = 1
+
+
 
         elif event.type == SDL_KEYUP:
             if event.key == SDLK_UP or event.key == SDLK_DOWN:
@@ -110,9 +90,39 @@ def handle_events():
             elif event.key == SDLK_RIGHT or event.key == SDLK_LEFT:
                 rect.dir_x = 0
 
-def pause():
+
+
+time = 0
+def update():
+    global time, drawing
+    for game_object in game_world.all_objects():
+        game_object.update()
+    if collide(rect, boss_face):
+        rect.hp -= 1
+        print("COLLISION")
+
+    # stage2로 넘어감
+    if time > 10.0:
+        logo_time = 0
+        game_framework.change_state(stage2_image)
+    time += 0.01
     pass
 
-def resume():
-    pass
+def draw():
+    clear_canvas()
+    for game_object in game_world.all_objects():
+        game_object.draw()
+    update_canvas()
+
+
+def collide(a, b):
+    left_a, bottom_a, right_a, top_a = a.get_bb()
+    left_b, bottom_b, right_b, top_b = b.get_bb()
+
+    if left_a > right_b: return False
+    if right_a < left_b: return False
+    if top_a < bottom_b: return False
+    if bottom_a > top_b: return False
+
+    return True
 
