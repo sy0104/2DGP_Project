10f9791d114eb1796f2fddcd4abffef76280f7af
soyeong park <syys0104@gmail.com>
2019-11-23 22:11:07 +0900

    rect
    충돌시 4초동안 보호막 추가

--- rect.py
+++ rect.py
@@ -6,12 +6,16 @@ class Rect:
     def __init__(self):
         self.image_hp2 = load_image('Resource\\image\\player_hp2.png')
         self.image_hp1 = load_image('Resource\\image\\player_hp1.png')
+        self.image_protected = load_image('Resource\\image\\protected_player.png')
         self.x, self.y = 100, 300
         self.frame = 0
         self.time = 0
         self.hp = 2
         self.dir_x = 0
         self.dir_y = 0
+        self.isCollide = False
+        self.protecting = False
+        self.protecting_time = 0
 
     def update(self):
         self.x += self.dir_x
@@ -20,14 +24,28 @@ class Rect:
             self.dir_x = 0
         if self.y >= 685 or self.y <= 15:
             self.dir_y = 0
+
+        if self.isCollide:
+            self.protecting = True
+            self.protecting_time += 0.01
+            if self.protecting_time > 4.0:
+                self.isCollide = False
+                self.protecting = False
+                self.protecting_time = 0.0
+
         pass
 
+    # hp == 2일땐 큰 rect / hp == 1일땐 작은 rect
+    # protecting 중에는 rect 색 하얗게
     def draw(self):
-        if self.hp == 2:
+        if self.hp == 2 and not self.protecting:
             self.image_hp2.clip_draw(0, 0, 30, 30, self.x, self.y)
-        else:
+        elif self.hp < 2 and not self.protecting:
             self.image_hp1.clip_draw(0, 0, 15, 15, self.x, self.y)
+        elif self.protecting:
+            self.image_protected.clip_draw(0, 0, 15, 15, self.x, self.y)
 
+    # 충돌체크
     def get_bb(self):
         if self.hp == 2:
             return self.x - 15, self.y - 15, self.x + 15, self.y + 15
