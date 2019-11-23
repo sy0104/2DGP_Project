07768773903783cace02847441f534850207712a
soyeong park <syys0104@gmail.com>
2019-11-24 03:34:40 +0900

    rect
    hp 3으로 수정

--- rect.py
+++ rect.py
@@ -4,13 +4,14 @@ import game_framework
 # 플레이어 캐릭터
 class Rect:
     def __init__(self):
+        self.image_hp3 = load_image('Resource\\image\\player_hp3.png')
         self.image_hp2 = load_image('Resource\\image\\player_hp2.png')
         self.image_hp1 = load_image('Resource\\image\\player_hp1.png')
         self.image_protected = load_image('Resource\\image\\protected_player.png')
         self.x, self.y = 100, 300
         self.frame = 0
         self.time = 0
-        self.hp = 2
+        self.hp = 3
         self.dir_x = 0
         self.dir_y = 0
         self.isCollide = False
@@ -35,20 +36,24 @@ class Rect:
 
         pass
 
-    # hp == 2일땐 큰 rect / hp == 1일땐 작은 rect
-    # protecting 중에는 rect 색 하얗게
+    # hp 작을수록 rect 크기 작아짐
+    # protecting 중에는 rect 색 바꿈
     def draw(self):
-        if self.hp == 2 and not self.protecting:
-            self.image_hp2.clip_draw(0, 0, 30, 30, self.x, self.y)
-        elif self.hp < 2 and not self.protecting:
+        if self.hp == 3 and not self.protecting:
+            self.image_hp3.clip_draw(0, 0, 30, 30, self.x, self.y)
+        elif self.hp == 2 and not self.protecting:
+            self.image_hp2.clip_draw(0, 0, 20, 20, self.x, self.y)
+        elif self.hp == 1 and not self.protecting:
             self.image_hp1.clip_draw(0, 0, 15, 15, self.x, self.y)
         elif self.protecting:
             self.image_protected.clip_draw(0, 0, 15, 15, self.x, self.y)
 
     # 충돌체크
     def get_bb(self):
-        if self.hp == 2:
+        if self.hp == 3:
             return self.x - 15, self.y - 15, self.x + 15, self.y + 15
+        elif self.hp == 2:
+            return self.x - 10, self.y - 10, self.x + 10, self.y + 10
         else:
             return self.x - 7.5, self.y - 7.5, self.x + 7.5, self.y + 7.5
 
