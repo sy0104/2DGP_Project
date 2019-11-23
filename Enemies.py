02d99d95d9a0ef80cd3318db2baf96b9da51adaa
soyeong park <syys0104@gmail.com>
2019-11-23 23:24:16 +0900

    Enemies
    smallest enemies 시작위치 랜덤으로

--- Enemies.py
+++ Enemies.py
@@ -1,21 +1,48 @@
 from pico2d import *
 import random
 
+# start_place
+# 1)    x = random,        y = 0 + pivot
+# 2)    x = 0 + pivot,     y = random
+# 3)    x = random,        y = 700 - pivot
+# 4)    x = 1280 - pivot,  y = random
+
 
 class SmallestEnemy:
     def __init__(self):
         self.image = load_image('Resource\\image\\smallest_enemy.png')
-        self.x, self.y = random.randint(100, 1600), random.randint(100, 600)
-        self.speed_x = random.randint(1, 5)
-        self.speed_y = random.randint(1, 5)
-        self.dir = random.randint(0, 1)
-        self.drawing = 0
+        self.x, self.y = random.randint(3, 1277), random.randint(3, 697)
+        self.speed = random.randint(1, 3)
+        self.dir = 1
+        self.start_place = random.randint(1, 4)
+
+        if self.start_place == 1:
+            self.x, self.y = random.randint(5, 1275), 5
+        elif self.start_place == 2:
+            self.x, self.y = 5, random.randint(5, 695)
+        elif self.start_place == 3:
+            self.x, self.y = random.randint(5, 1275), 695
+        elif self.start_place == 4:
+            self.x, self.y = 1275, random.randint(5, 695)
 
     def update(self):
-        if self.dir == 0:
-            self.x += self.speed_x
-        else:
-            self.y += self.speed_y
+        if self.start_place == 1:
+            self.y += self.speed * self.dir
+            if self.y >= 700-2.5 or self.y <= 0-2.5:
+                self.dir *= -1
+        elif self.start_place == 2:
+            self.x += self.speed * self.dir
+            if self.x >= 1280-2.5 or self.x <= 0+2.5:
+                self.dir *= -1
+        elif self.start_place == 3:
+            self.y -= self.speed * self.dir
+            if self.y <= 0+2.5 or self.y >= 700-2.5:
+                self.dir *= -1
+        elif self.start_place == 4:
+            self.x -= self.speed
+            if self.x >= 0+2.5 or self.x <= 1280-2.5:
+                self.dir *= -1
+
 
     def draw(self):
         self.image.clip_draw(0, 0, 5, 5, self.x, self.y)
@@ -27,11 +54,10 @@ class SmallestEnemy:
 class SmallEnemy:
     def __init__(self):
         self.image = load_image('Resource\\image\\small_enemy.png')
-        self.x, self.y = random.randint(100, 1600), random.randint(100, 600)
+        self.x, self.y = random.randint(15, 1265), random.randint(15, 685)
         self.speed_x = random.randint(1, 5)
         self.speed_y = random.randint(1, 5)
-        self.dir = random.randint(0, 1)
-        self.drawing = 0
+        self.dir = 1
 
     def update(self):
         if self.dir == 0:
@@ -49,11 +75,10 @@ class SmallEnemy:
 class BigEnemy:
     def __init__(self):
         self.image = load_image('Resource\\image\\big_enemy.png')
-        self.x, self.y = random.randint(100, 1600), random.randint(100, 600)
+        self.x, self.y = random.randint(30, 1250), random.randint(30, 670)
         self.speed_x = random.randint(1, 5)
         self.speed_y = random.randint(1, 5)
-        self.dir = random.randint(0, 1)
-        self.drawing = 0
+        self.dir = 1
 
     def update(self):
         if self.dir == 0:
