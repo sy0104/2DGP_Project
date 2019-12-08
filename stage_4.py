a29d34a21051f57e1ffe42def5f80251896e3604
soyeong park <syys0104@gmail.com>
2019-12-09 03:37:42 +0900

    stage 4
    stage 4 - 30초

--- stage_4.py
+++ stage_4.py
@@ -9,6 +9,8 @@ import clear_image
 import gameover_image
 from rect import Rect
 from Boss import MovingBoss
+from BackgroundAttack import UpDownWall, LeftRightWall
+from Enemies import BouncingEnemy, CircleEnemy, SmallestEnemy
 
 name = "stage_4"
 
@@ -17,18 +19,6 @@ name = "stage_4"
 class Background:
     def __init__(self):
         self.image = load_image('Resource\\image\\background_image.png')
-
-    def draw(self):
-        self.image.draw(1280 // 2, 700 // 2)
-
-    def update(self):
-        pass
-
-
-# 세모 벽
-class TriangleWall:
-    def __init__(self):
-        self.image = load_image('Resource\\image\\triangle_wall.png')
 
     def draw(self):
         self.image.draw(1280 // 2, 700 // 2)
@@ -46,13 +36,31 @@ def enter():
     background = Background()
     game_world.add_object(background, 0)
 
-    global triangle_wall
-    triangle_wall = TriangleWall()
-    game_world.add_object(triangle_wall, 0)
-
     global moving_boss
     moving_boss = MovingBoss()
     game_world.add_object(moving_boss, 0)
+
+    global up_down_wall
+    up_down_wall = UpDownWall()
+    game_world.add_object(up_down_wall, 1)
+
+    global left_right_wall
+    left_right_wall = LeftRightWall()
+    game_world.add_object(left_right_wall, 1)
+
+    global bouncing_enemies
+    bouncing_enemies = [BouncingEnemy() for k in range(5)]
+    game_world.add_objects(bouncing_enemies, 0)
+
+    global circles
+    circles = [CircleEnemy() for w in range(7)]
+    game_world.add_objects(circles, 0)
+
+    global smallest_enemies
+    smallest_enemies = [SmallestEnemy() for i in range(15)]
+    game_world.add_objects(smallest_enemies, 0)
+
+
 
 def exit():
     game_world.clear()
@@ -66,12 +74,17 @@ def draw():
 
 
 next_stage_time = 0
-
+start_protecting = 0
 
 def update():
-    global next_stage_time
+    global next_stage_time, start_protecting
     for game_object in game_world.all_objects():
         game_object.update()
+
+    # 처음 시작할때 잠시 무적
+    start_protecting += 0.01
+    if start_protecting <= 2.0:
+        rect.isCollide = True
 
     # rect & moving boss 충돌체크
     if collide(rect, moving_boss) and not rect.isCollide:
@@ -79,10 +92,43 @@ def update():
         rect.hp -= 1
         print("rect & moving boss COLLISION")
 
+    # rect & wall 충돌체크
+    if collide(rect, up_down_wall) and not rect.isCollide:
+        rect.isCollide = True
+        rect.hp -= 1
+        print("rect & wall COLLISION")
+
+    if collide(rect, left_right_wall) and not rect.isCollide:
+        rect.isCollide = True
+        rect.isCollide = True
+        rect.hp -= 1
+        print("rect & wall COLLISION")
+
+    # rect & bouncing ball 충돌체크
+    for bouncing in bouncing_enemies:
+        if collide(bouncing, rect) and not rect.isCollide:
+            rect.isCollide = True
+            rect.hp -= 1
+            print("rect & bouncing enemy COLLISION")
+
+    # rect & circle 충돌체크
+    for circle in circles:
+        if collide(circle, rect) and not rect.isCollide:
+            rect.isCollide = True
+            rect.hp -= 1
+            print("rect & circle COLLISION")
+
+    # rect & smallest ball 충돌체크
+    for smallest in smallest_enemies:
+        if collide(smallest, rect) and not rect.isCollide:
+            rect.isCollide = True
+            rect.hp -= 1
+            print("rect & smallest enemy COLLISION")
+
     # clear 로 넘어감
-    if next_stage_time > 10000.0:
-        game_framework.change_state(clear_image)
     next_stage_time += 0.01
+    if next_stage_time > 80.0:
+        game_framework.change_state(clear_image)
 
     # rect.hp == 0이 되면 game over
     if rect.hp <= 0:
