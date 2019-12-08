5984f05971597413d45f675a1c9521a71c24a5e5
soyeong park <syys0104@gmail.com>
2019-12-09 03:36:25 +0900

    Boss
    Boss objects

--- Boss.py
+++ Boss.py
@@ -17,40 +17,6 @@ class BossFace:
         return self.x - 140, self.y - 140, self.x + 140, self.y + 140
 
 
-# stage 3 - 보스 팔 (좌우)
-# left arm
-class BossLeftArm:
-    def __init__(self):
-        self.image = load_image('Resource\\image\\boss_arm_left.png')
-        self.x, self.y = 450, 200
-
-    def update(self):
-        pass
-
-    def draw(self):
-        self.image.clip_draw(0, 0, 212, 471, self.x, self.y)
-
-    def get_bb(self):
-        return self.x - 106, self.y - 235, self.x + 106, self.y + 235
-
-
-
-# right arm
-class BossRightArm:
-    def __init__(self):
-        self.image = load_image('Resource\\image\\boss_arm_right.png')
-        self.x, self.y = 830, 200
-
-    def update(self):
-        pass
-
-    def draw(self):
-        self.image.clip_draw(0, 0, 212, 417, self.x, self.y)
-
-    def get_bb(self):
-        return self.x - 106, self.y - 235, self.x + 106, self.y + 235
-
-
 # stage 3 - 우는 보스
 class CryingBoss:
     def __init__(self):
@@ -73,31 +39,18 @@ class LongTear:
         self.image = load_image('Resource\\image\\tear_long.png')
         self.eye = random.randint(0, 1)
         self.time = 0
-
-        # 왼쪽 눈에서 떨어짐
-        if self.eye == 0:
-            self.x = random.randint(560, 600)
-            self.y = 500
-        # 오른쪽 눈에서 떨어짐
-        elif self.eye == 1:
-            self.x = random.randint(590, 710)
-            self.y = 460
-
+        self.x, self.y = 570, 485
         self.speed = 0.5
 
     def update(self):
-        self.eye = random.randint(0, 1)
         self.y -= self.speed
         if self.y <= 350:
-            self.speed = 2
+            self.speed = random.randint(1, 3)
         elif self.y > 350:
             self.speed = 0.5
 
-        if self.y - 50 <= 0:
-            if self.eye == 0:
-                self.y = 500
-            elif self.eye == 1:
-                self.y = 460
+        if self.y <= -110:
+            self.y = 485
 
     def draw(self):
         self.image.clip_draw(0, 0, 20, 100, self.x, self.y)
@@ -110,33 +63,20 @@ class LongTear:
 class ShortTear:
     def __init__(self):
         self.image = load_image('Resource\\image\\tear_short.png')
-        self.eye = random.randint(0, 1)
         self.time = 0
-
-        # 왼쪽 눈에서 떨어짐
-        if self.eye == 0:
-            self.x = random.randint(560, 600)
-            self.y = 500
-        # 오른쪽 눈에서 떨어짐
-        elif self.eye == 1:
-            self.x = random.randint(590, 710)
-            self.y = 460
+        self.x, self.y = 700, 485
 
         self.speed = 0.5
 
     def update(self):
-        self.eye = random.randint(0, 1)
         self.y -= self.speed
         if self.y <= 350:
-            self.speed = 2
+            self.speed = random.randint(1, 3)
         elif self.y > 350:
             self.speed = 0.5
 
-        if self.y - 35 <= 0:
-            if self.eye == 0:
-                self.y = 500
-            elif self.eye == 1:
-                self.y = 460
+        if self.y <= -80:
+            self.y = 485
 
     def draw(self):
         self.image.clip_draw(0, 0, 20, 70, self.x, self.y)
@@ -145,11 +85,11 @@ class ShortTear:
         return self.x - 10, self.y - 35, self.x + 10, self.y + 35
 
 
-# stage 4(final) - 팔 움직이는 보스
+# stage 4(final) - 팔 내리치는 보스
 class MovingBoss:
     def __init__(self):
         self.image = load_image('Resource\\image\\moving_boss.png')
-        self.x, self.y = 640, 400
+        self.x, self.y = 640, 250
         self.frame = 0
         self.time = 0
 
@@ -162,4 +102,6 @@ class MovingBoss:
         self.image.clip_draw(self.frame * 600, 0, 600, 600, self.x, self.y)
 
     def get_bb(self):
-        return self.x - 250, self.y - 250, self.x + 250, self.y + 250
+        return self.x - 250, self.y - 300, self.x + 250, self.y + 250
+
+
